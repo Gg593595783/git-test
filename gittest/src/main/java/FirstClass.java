2d407e9a00e1834f091299fc4185a2a83fe4b8ff
zhao <zhao@qq.com>
2019-03-29 16:15:07 +0800

    把本地工程添加到git

--- gittest/src/main/java/FirstClass.java
+++ gittest/src/main/java/FirstClass.java
@@ -6,4 +6,8 @@ public class FirstClass {
         System.out.println("hello gey!");
         System.out.println("hello gey!");
     }
+    public void test(){
+        int i=10;
+        i+=0;
+    }
 }
